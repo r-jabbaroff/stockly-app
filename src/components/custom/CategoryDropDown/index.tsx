126998fa80dabe4c7ba1025ff4d6ff22aa61e915
System Administrator <root@Rostams-MacBook-Air.local>
2024-12-12 22:23:43 +0300

    ESLint Errors Fixed

--- src/components/custom/CategoryDropDown/index.tsx
+++ src/components/custom/CategoryDropDown/index.tsx
@@ -81,8 +81,8 @@ export function CategoriesDropDown({
           </Button>
         </PopoverTrigger>
         <PopoverContent className="poppins w-56 p-0" side="bottom" align="end">
+          {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
           <Command className="p-1">
-            <>
               <CommandInput placeholder="Category" />
               <CommandList>
                 <CommandEmpty className="p-5 text-center text-sm text-slate-500">
@@ -115,7 +115,6 @@ export function CategoriesDropDown({
                   Clear Filters
                 </Button>
               </div>
-            </>
           </Command>
         </PopoverContent>
       </Popover>
